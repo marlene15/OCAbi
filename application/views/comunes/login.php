fb5ec36a8dad63083229227d1e70410861cf681a
marlene15 <mar106ale@hotmail.com>
2016-01-20 14:09:24 -0600

    Cambios en Inicio

--- application/views/comunes/login.php
+++ application/views/comunes/login.php
@@ -81,7 +81,7 @@
 	<!-- END LOGIN -->
 	<!-- BEGIN COPYRIGHT -->
 	<div class="copyright">
-		2015 &copy; OCA
+		2016 &copy; OCAdos
 	</div>
 	<!-- END COPYRIGHT -->
 	<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
